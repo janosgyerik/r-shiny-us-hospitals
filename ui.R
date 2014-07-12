cc048de057ae9953228a12c78f8b7f06aeac4f38
Janos Gyerik <janos@kronos>
2014-07-13 00:24:45 +0200

    init

---
+++ ui.R
@@ -0,0 +1,26 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("US Hospitals"),
+  
+  sidebarPanel(
+    sliderInput("obs", 
+                "Number of observations:", 
+                min = 1, 
+                max = 1000, 
+                value = 500)
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("distPlot")
+  )
+))
